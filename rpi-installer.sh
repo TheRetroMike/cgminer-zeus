b546d13f37aaeb4ec500f7088d3d8ba0a0deb2a1
RetroMike <michaeldlesk@gmail.com>
2024-08-23 18:26:01 -0500

    Update rpi-installer.sh

--- rpi-installer.sh
+++ rpi-installer.sh
@@ -3,8 +3,8 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt-get install build-essential autoconf automake libtool pkg-config libcurl4-openssl-dev libudev-dev libjansson-dev libncurses5-dev git libzip-dev -y
-git clone https://github.com/wareck/cgminer-lketc.git
-cd cgminer-lketc
+git clone https://github.com/TheRetroMike/cgminer-zeus.git
+cd cgminer-zeus
 sudo usermod -a -G dialout,plugdev $USER
 sudo cp 01-cgminer.rules /etc/udev/rules.d/
 CFLAGS="-O2 -march=native" ./autogen.sh
@@ -12,7 +12,7 @@ CFLAGS="-O2 -march=native" ./autogen.sh
 make
 sudo mv cgminer /usr/local/bin/cgminer
 cd ..
-sudo rm -R cgminer-lketc
+sudo rm -R cgminer-zeus
 sudo echo '#!/bin/bash' | sudo tee /etc/startup.sh
 sudo echo "screen -dmS miner sudo /usr/local/bin/cgminer --scrypt -o stratum+tcp://americas.mining-dutch.nl:8888 -u 05sonicblue.donation -p d=128 --zeus-chips 6 --zeus-clock 328" | sudo tee -a /etc/startup.sh
 sudo chmod +x /etc/startup.sh
